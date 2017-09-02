9941683913ad93b348868fffec63f39b3e422bb8
Filip Kręcichwost <filip.ffk@gmail.com>
2017-09-02 16:44:30 +0200

    add 'catch all routes' for dev server

--- server/server.development.js
+++ server/server.development.js
@@ -2,9 +2,12 @@ module.exports = (express, app) => {
   const webpack = require('webpack');
   const webpackConfig = require('../webpack.config');
   const compiler = webpack(webpackConfig);
+  const history = require('connect-history-api-fallback');
+
+  app.use(history());
 
   app.use(require("webpack-dev-middleware")(compiler, {
-    noInfo: false, publicPath: webpackConfig.output.publicPath
+    noInfo: false, publicPath: webpackConfig.output.publicPath, historyApiFallback: true,
   }));
 
   app.use(require("webpack-hot-middleware")(compiler));
