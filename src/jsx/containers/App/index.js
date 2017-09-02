22e7c5fe2c2eb182c23c577a903f5dc7caaf7e88
Filip Kręcichwost <filip.ffk@gmail.com>
2017-09-02 16:28:32 +0200

    Add Menu into App

--- src/jsx/containers/App/index.js
+++ src/jsx/containers/App/index.js
@@ -2,17 +2,29 @@ import React from 'react';
 import stores, { history } from 'stores';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-
+import { Container, Row, Col } from 'reactstrap';
+import Menu from 'jsx/components/Menu';
 import './style.scss';
 
-export default () => (
+const routes = [
+  { title: 'One', path: '/one', component: () => null },
+  { title: 'Two', path: '/two', component: () => <span>two</span> },
+];
+
+const App = () => (
   <div className="app">
     <Provider store={stores}>
       <ConnectedRouter history={history}>
-        <div>
-          Helloł
-        </div>
+        <Container>
+          <Row>
+            <Col sm="3">
+              <Menu items={routes} />
+            </Col>
+          </Row>
+        </Container>
       </ConnectedRouter>
     </Provider>
   </div>
 );
+
+export default App;
