e6bbdbd79c13c6146261cf5ed8414fbed8ac2598
Filip Kręcichwost <filip.ffk@gmail.com>
2017-09-02 20:49:21 +0200

    add abstraction for console.log

---
+++ src/services/logger.js
@@ -0,0 +1,9 @@
+/* simple abstraction layer for logger */
+
+/* eslint-disable no-console */
+export default {
+  log: console.log,
+  warn: console.warn,
+  error: console.error,
+};
+/* eslint-enable no-console */
