c99a2b552bbf9aa856030bbd9ae16144536d7103
Filip Kręcichwost <filip.ffk@gmail.com>
2017-09-02 20:35:21 +0200

    add request service

---
+++ src/services/request.js
@@ -0,0 +1,38 @@
+import queryString from 'querystring';
+
+const requestMethods = ['get', 'post', 'put', 'patch', 'delete'];
+const request = {};
+const apiEndpoint = process.env.API_ADDRESS;
+
+requestMethods.forEach(requestMethod => {
+  request[requestMethod] = function (endpoint, params) {
+    let address = endpoint;
+    const requestObj = {
+      method: requestMethod.toUpperCase(),
+    };
+
+    if (params.body) {
+      requestObj.body = JSON.stringify(params.body || null);
+    }
+
+    if (params.query) {
+      address += '?' + queryString.stringify(params.query);
+    }
+
+    return window.fetch(apiEndpoint + address)
+      .then(response => {
+        if (response.ok && response.status >= 200 && response.status < 300) {
+          if (response.status === 204) {
+            return true;
+          }
+
+          return response.json()
+        }
+
+        return response.text()
+          .then(result => Promise.reject(result));
+      });
+  }
+});
+
+export default request;
