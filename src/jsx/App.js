368463d70783b2d681af55b51f3d68ed469f9d6d
Filip Kręcichwost <filip.ffk@gmail.com>
2017-09-02 14:14:32 +0200

    connect redux with react (by provider)

--- src/jsx/App.js
+++ src/jsx/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import stores, { history } from 'stores';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
 
 export default () => (
   <div className="app">
-    My Component
+    <Provider store={stores}>
+      <ConnectedRouter history={history}>
+
+      </ConnectedRouter>
+    </Provider>
   </div>
 );
