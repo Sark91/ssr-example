eb0a5f16b8ddd6ccdc2ca6d1475775fe069f0800
Filip Kręcichwost <filip.ffk@gmail.com>
2017-09-02 18:13:04 +0200

    finish Menu Component

--- __tests__/jsx/components/Menu/index.js
+++ __tests__/jsx/components/Menu/index.js
@@ -1,37 +1,51 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { Menu } from 'jsx/components/Menu';
+import { spy } from 'sinon';
+
+const mountMenuElement = (hideInMenu = false, push = () => {}) => mount(
+  <Menu
+    location={{ pathname: '/' }}
+    history={{ push }}
+    items={[
+      { title: 'One', path: '/', component: () => null, hideInMenu },
+      { title: 'Two', path: '/index', component: () => null , hideInMenu },
+    ]}
+  />
+);
 
 describe('<Menu/> Component', () => {
   test('It should mount properly with className="menu"', () => {
-    const MenuElement = shallow(<Menu />);
+    const MenuElement = mountMenuElement();
 
     expect(MenuElement.find('.menu').length).toBe(1);
   });
 
   test('It should contains items, when prop items is not empty', () => {
-    const MenuElement = mount(
-      <Menu
-        items={[
-          { title: 'One', path: '/', component: () => null },
-          { title: 'Two', path: '/index', component: () => null },
-        ]}
-      />
-    );
+    const MenuElement = mountMenuElement();
 
     expect(MenuElement.find('.list-group-item').length).toBe(2);
   });
 
   test('It should properly handle item with "hideInMenu" property', () => {
-    const MenuElement = mount(
-      <Menu
-        items={[
-          { title: 'One', path: '/', component: () => null, hideInMenu: true, },
-          { title: 'Two', path: '/index', component: () => null , hideInMenu: true,},
-        ]}
-      />
-    );
+    const MenuElement = mountMenuElement(true);
 
     expect(MenuElement.find('.list-group-item').length).toBe(0);
+  });
+
+  test('It should be active when item.path equals location.pathname prop', () => {
+    const MenuElement = mountMenuElement();
+
+    expect(MenuElement.find('ul.menu').childAt(0).find('.active').length).toBe(1);
+    expect(MenuElement.find('ul.menu').childAt(1).find('.active').length).toBe(0);
+  });
+
+  test('It should call func in history.push property', () => {
+    const callback = spy();
+    const MenuElement = mountMenuElement(false, callback).find('ul.menu').childAt(0).find('li');
+
+    MenuElement.simulate('click');
+
+    expect(callback.called).toBe(true);
   });
 });
--- src/jsx/components/Menu/index.js
+++ src/jsx/components/Menu/index.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import { withRouter } from 'react-router';
 
-const Menu = ({ items }) => (
+const isItemActive = (item, location) => item.path === location.pathname;
+
+const Menu = ({ items, location, history: { push } }) => (
   <div>
     <ListGroup className="menu">
       {items
@@ -11,10 +13,8 @@ const Menu = ({ items }) => (
         .map(item => (
           <ListGroupItem
             key={item.path}
-            active
-            tag="a"
-            href={item.path}
-            action
+            active={isItemActive(item, location)}
+            onClick={() => push(item.path)}
           >
             {item.title}
           </ListGroupItem>
@@ -30,6 +30,12 @@ Menu.propTypes = {
     component: PropTypes.any.isRequired, // @fixme: verify proptype for element class
     title: PropTypes.string.isRequired,
   })),
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 Menu.defaultProps = {
