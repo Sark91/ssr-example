8756ec729f57d977a14ac28941af38451f724f59
Filip Kręcichwost <filip.ffk@gmail.com>
2017-09-03 12:34:00 +0200

    add mock store

---
+++ __tests__/stores/_mockStore.js
@@ -0,0 +1,8 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import clientMiddleware from 'stores/middlewares/clientMiddleware';
+import request from 'services/request';
+
+const middlewares = [thunk, clientMiddleware(request)];
+
+export default () => configureMockStore(middlewares);
