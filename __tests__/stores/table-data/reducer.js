fa24fcd0ff1463d3f5bad0a9121c95cb42f632a0
Filip Kręcichwost <filip.ffk@gmail.com>
2017-09-03 12:33:36 +0200

    add table-data actions

---
+++ __tests__/stores/table-data/reducer.js
@@ -0,0 +1,135 @@
+import {
+  createDataState,
+  isTableDataActionType,
+  reducer,
+} from 'stores/table-data/reducer';
+
+import * as tableDataActionTypes from 'stores/table-data/actionTypes';
+
+describe('table-data reducer', () => {
+  test('"createDataState" should return correct data shape', () => {
+    expect(createDataState({ endpoint: '/posts' })).toMatchObject({
+      endpoint: '/posts',
+      query: {
+        _page: expect.any(Number),
+        _limit: expect.any(Number),
+      },
+      status: 'success',
+      error: null,
+      data: expect.any(Array),
+    });
+  });
+
+  test('"isTableDataActionType" should return true on every table data action type', () => {
+    for (const actionType in tableDataActionTypes) {
+      expect(isTableDataActionType(actionType)).toBe(true);
+    }
+  });
+
+  test('"reducer" - TABLE_DATA_GET_DATA', () => {
+    const mockedStore = {
+      all: createDataState({ endpoint: '/posts' }),
+    };
+
+    const action = {
+      type: tableDataActionTypes.TABLE_DATA_GET_DATA,
+      payload: { prop: 'all' },
+    };
+
+    const newState = reducer(mockedStore, action);
+
+    expect(mockedStore === newState).toBe(false);
+    expect(newState.all).toMatchObject({
+      status: 'fetch',
+      data: [],
+      error: null,
+    });
+  });
+
+  test('"reducer" - TABLE_DATA_GET_DATA_SUCCESS', () => {
+    const mockedStore = {
+      all: {
+        ...createDataState({ endpoint: '/posts' }),
+        status: 'other',
+      },
+    };
+
+    const action = {
+      type: tableDataActionTypes.TABLE_DATA_GET_DATA_SUCCESS,
+      payload: { prop: 'all' },
+      result: [1],
+    };
+
+    const newState = reducer(mockedStore, action);
+
+    expect(mockedStore === newState).toBe(false);
+    expect(newState.all).toMatchObject({
+      status: 'success',
+      data: [1],
+      error: null,
+    });
+  });
+
+  test('"reducer" - TABLE_DATA_GET_DATA_ERROR', () => {
+    const mockedStore = {
+      all: {
+        ...createDataState({ endpoint: '/posts' }),
+        status: 'other',
+      },
+    };
+
+    const action = {
+      type: tableDataActionTypes.TABLE_DATA_GET_DATA_ERROR,
+      payload: { prop: 'all' },
+      error: 'error',
+    };
+
+    const newState = reducer(mockedStore, action);
+
+    expect(mockedStore === newState).toBe(false);
+    expect(newState.all).toMatchObject({
+      status: 'error',
+      error: 'error',
+    });
+  });
+
+  test('"reducer" - TABLE_DATA_SET_LIMIT', () => {
+    const mockedState = {
+      all: createDataState({ endpoint: '/posts' }),
+    };
+
+    const action = {
+      type: tableDataActionTypes.TABLE_DATA_SET_LIMIT,
+      payload: { prop: 'all', _limit: -1 },
+    };
+
+    const newState = reducer(mockedState, action);
+
+    expect(mockedState === newState).toBe(false);
+    expect(newState.all.query._limit).toBe(-1);
+
+    newState.all.query._limit = mockedState.all.query._limit;
+
+    expect(mockedState).toMatchObject(newState); // check reducer changed only _limit
+  });
+
+  test('"reducer" - TABLE_DATA_SET_PAGE', () => {
+    const mockedState = {
+      all: createDataState({ endpoint: '/posts' }),
+    };
+
+    const action = {
+      type: tableDataActionTypes.TABLE_DATA_SET_PAGE,
+      payload: { prop: 'all', _page: -1 },
+    };
+
+    const newState = reducer(mockedState, action);
+
+    expect(mockedState === newState).toBe(false);
+    expect(newState.all.query._page).toBe(-1);
+
+    newState.all.query._page = mockedState.all.query._page;
+
+    expect(mockedState).toMatchObject(newState); // check reducer changed only _page
+  });
+});
---
+++ src/stores/table-data/reducer.js
@@ -0,0 +1,99 @@
+/**
+ * Because all fetched data have the same shape i made this common reducer part to not repeat myself
+ */
+
+// eslint-disable-next-line import/no-duplicates
+import * as tableDataActionTypes from 'stores/table-data/actionTypes';
+import {
+  TABLE_DATA_GET_DATA,
+  TABLE_DATA_GET_DATA_SUCCESS,
+  TABLE_DATA_GET_DATA_ERROR,
+
+  TABLE_DATA_SET_LIMIT,
+  TABLE_DATA_SET_PAGE,
+} from 'stores/table-data/actionTypes'; // eslint-disable-line import/no-duplicates
+import immutable from 'object-path-immutable';
+
+
+/**
+ * Create part of state with data used to fetching data
+ * @see how its used in 'posts' reducer
+ * @param endpoint
+ */
+const createDataState = ({ endpoint }) => ({
+  endpoint,
+  query: {
+    _page: 1,
+    _limit: 12,
+  },
+  status: 'success', // statuses: fetch, success, error. If endless scrolling append_fetch, append_success, append_error
+  error: null,
+  data: [],
+});
+
+/**
+ * @see how its used in 'posts' reducer
+ * @param actionTable
+ */
+const isTableDataActionType = actionTable => actionTable in tableDataActionTypes;
+
+/**
+ * Reducer part.
+ * @see how its used in 'posts' reducer
+ * @param state - state given from reducer
+ * @param action - action given from reducer
+ * @returns {{}} - modified state
+ */
+const reducer = (state, action) => {
+  switch (action.type) {
+    case TABLE_DATA_GET_DATA:
+      return {
+        ...state,
+        [action.payload.prop]: {
+          ...state[action.payload.prop],
+          status: 'fetch',
+          data: [],
+          error: null,
+        },
+      };
+
+    case TABLE_DATA_GET_DATA_SUCCESS:
+      return {
+        ...state,
+        [action.payload.prop]: {
+          ...state[[action.payload.prop]],
+          status: 'success',
+          data: action.result,
+          error: null,
+        },
+      };
+
+    case TABLE_DATA_GET_DATA_ERROR:
+      return {
+        ...state,
+        [action.payload.prop]: {
+          ...state[[action.payload.prop]],
+          status: 'error',
+          data: [],
+          error: action.error,
+        },
+      };
+
+    case TABLE_DATA_SET_LIMIT:
+      // eslint-disable-next-line no-underscore-dangle
+      return immutable.set(state, `${action.payload.prop}.query._limit`, action.payload._limit);
+
+    case TABLE_DATA_SET_PAGE:
+      // eslint-disable-next-line no-underscore-dangle
+      return immutable.set(state, `${action.payload.prop}.query._page`, action.payload._page);
+
+    default:
+      return state;
+  }
+};
+
+export {
+  createDataState,
+  isTableDataActionType,
+  reducer,
+};
